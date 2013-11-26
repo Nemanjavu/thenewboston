888efb3aa35cb5f323e59cf3cfb4b0d973f856be
nemi <nemi@nemi-PC>
2013-11-26 22:28:46 +0100

    bucky

---
+++ TheNewBoston/src/com/example/thenewboston/Splash.java
@@ -0,0 +1,35 @@
+package com.example.thenewboston;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.splash);
+		Thread timer = new Thread() {
+			public void run() {
+				try {
+					sleep(5000);
+
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+					// TODO: handle exception
+				} finally {
+					Intent intent = new Intent(
+							"com.example.thenewboston.STARTINGPOINT");
+					startActivity(intent);
+
+				}
+
+			}
+
+		};
+		timer.start();
+	}
+
+}
